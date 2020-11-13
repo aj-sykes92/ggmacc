9bcb8fc215d608fbfd22a1ea07a6d71037e9727c
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-11-13 20:51:01 +0000

    Fixed all warnings and notes.

--- R/geom_macc.R
+++ R/geom_macc.R
@@ -5,12 +5,14 @@
 #' @param fill An optional parameter to specify fill groupings to be used by `geom_macc`.
 #' @param ... Any additional arguments to pass to `geom_rect`.
 #' @importFrom ggplot2 geom_rect
+#' @importFrom ggplot2 aes
+#' @export
 geom_macc <- function(fill = NULL, ...) {
   geom_rect(
-    aes(xmin = xmin,
-        xmax = xmax,
-        ymin = ymin,
-        ymax = ymax,
+    aes(xmin = .data$xmin,
+        xmax = .data$xmax,
+        ymin = .data$ymin,
+        ymax = .data$ymax,
         fill = {{ fill }}
     ),
     ...)
--- R/macc_prep.R
+++ R/macc_prep.R
@@ -7,6 +7,8 @@
 #' @param mac The name of the variable containing marginal abatement cost data.
 #' @param abatement The name of the variable containing abatement data.
 #' @import dplyr
+#' @importFrom rlang abort
+#' @export
 macc_prep <- function(data, mac, abatement) {
   if (is.null(data))
     abort("No data to prepare.")
@@ -18,7 +20,7 @@ macc_prep <- function(data, mac, abatement) {
   data %>%
     arrange({{ mac }}) %>%
     mutate(xmax = cumsum({{ abatement }}),
-           xmin = lag(xmax, default = 0),
+           xmin = lag(.data$xmax, default = 0),
            ymin = ifelse({{ mac }} < 0, {{ mac }}, 0),
            ymax = ifelse({{ mac }} > 0, {{ mac }}, 0))
 }
