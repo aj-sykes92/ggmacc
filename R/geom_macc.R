35baac79c6681b4f9d371da2df65badd5b97c1cb
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-11-13 21:33:14 +0000

    Tweak to geom_macc to allow alpha option to be spec'd in args.

--- R/geom_macc.R
+++ R/geom_macc.R
@@ -3,9 +3,10 @@
 #' a `ggplot` object to build a marginal abatement cost curve. A pre-parameterised wrapper for
 #' `geom_rect`.
 #' @param fill An optional parameter to specify fill groupings to be used by `geom_macc`.
+#' @param alpha An optional parameter to specify alpha (transparency) values to be used by
+#' `geom_macc`.
 #' @param ... Any additional arguments to pass to `geom_rect`.
-#' @importFrom ggplot2 geom_rect
-#' @importFrom ggplot2 aes
+#' @import ggplot2
 #' @export
 geom_macc <- function(fill = NULL, ...) {
   geom_rect(
@@ -13,7 +14,8 @@ geom_macc <- function(fill = NULL, ...) {
         xmax = .data$xmax,
         ymin = .data$ymin,
         ymax = .data$ymax,
-        fill = {{ fill }}
+        fill = {{ fill }},
+        alpha = {{ alpha }}
     ),
     ...)
 }
